4bd225d96c9129506746e4680b5abf346e63e9a0
distep <distep2@gmail.com>
2022-09-09 12:14:58 +0500

    add polymorphism based on oneOf and discriminator blocks

--- springwolfdoc2dto-core/src/main/java/io/github/stepanovd/springwolf2dto/PojoGenerator.java
+++ springwolfdoc2dto-core/src/main/java/io/github/stepanovd/springwolf2dto/PojoGenerator.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2010-2014 Nokia
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,16 +15,21 @@
  */
 package io.github.stepanovd.springwolf2dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.*;
 import org.jsonschema2pojo.*;
 import org.jsonschema2pojo.rules.RuleFactory;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
- *
  * @author Dmitry Stepanov (distep2@gmail.com)
  */
 public class PojoGenerator {
@@ -33,10 +38,10 @@ public class PojoGenerator {
 
     /**
      * Generate Java classes based on json schemas
-     * @param configuration configuration {@link Configuration}
-     * @param connector implementation of connector for extract json schemas
-     * @throws IOException i/o exception
      *
+     * @param configuration configuration {@link Configuration}
+     * @param connector     implementation of connector for extract json schemas
+     * @throws IOException i/o exception
      * @see HttpConnector
      * @see StringConnector
      */
@@ -73,7 +78,6 @@ public class PojoGenerator {
             }
 
 
-
             @Override
             public boolean isIncludeHashcodeAndEquals() {
                 return false;
@@ -83,22 +87,166 @@ public class PojoGenerator {
             public boolean isIncludeToString() {
                 return false;
             }
+
             @Override
             public boolean isIncludeGeneratedAnnotation() {
                 return true;
             }
+
+            @Override
+            public boolean isSerializable() {
+                return true;
+            }
+
+
         };
 
         Iterator<String> fieldNames = schemas.fieldNames();
-        while(fieldNames.hasNext()) {
-            String next = fieldNames.next();
 
-            String schema = schemas.get(next).toPrettyString();
+        Map<String, String> parentRefs = new HashMap<>();
+        Map<String, Map<String, String>> discriminatorsMapping = new HashMap<>();
 
-            SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
-            mapper.generate(jcodeModel, next, configuration.packageName(), schema);
+        while (fieldNames.hasNext()) {
+            String componentName = fieldNames.next();
 
-            jcodeModel.build(configuration.outputJavaClassDirectory().toFile());
+            JsonNode schemaNode = schemas.get(componentName);
+            String schema = schemaNode.toPrettyString();
+
+
+            RuleFactory ruleFactory = new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore());
+            SchemaMapper mapper = new SchemaMapper(ruleFactory, new SchemaGenerator());
+            JType jType = mapper.generate(jcodeModel, componentName, configuration.packageName(), schema);
+
+            //process references to child classes
+            if (schemaNode.has("oneOf")) {
+                JsonNode oneOf = schemaNode.get("oneOf");
+
+                Iterator<JsonNode> elements = oneOf.elements();
+                while (elements.hasNext()) {
+                    JsonNode childNode = elements.next();
+                    String childRef = childNode.get("$ref").asText("");
+                    childRef = childRef.substring(childRef.lastIndexOf("/") + 1);
+                    if (!childRef.isEmpty()) {
+                        parentRefs.put(childRef, componentName);
+                    }
+                }
+
+                ((JDefinedClass) jType).fields().forEach((fieldName, field) -> field.mods().setProtected());
+            }
+
+            //process discriminator
+            if (schemaNode.has("discriminator")) {
+                JsonNode discriminator = schemaNode.get("discriminator");
+                JsonNode propertyName = discriminator.get("propertyName");
+
+
+                tryToAnnotateJsonTypeInfo((JDefinedClass) jType, propertyName.asText());
+                if (discriminator.has("mapping")) {
+                    JsonNode mappingNode = discriminator.get("mapping");
+                    Iterator<String> discriminatorValues = mappingNode.fieldNames();
+
+                    Map<String, String> mapping = new HashMap<>();
+
+                    while (discriminatorValues.hasNext()) {
+                        String dValue = discriminatorValues.next();
+                        String mappingRef = mappingNode.get(dValue).asText();
+
+
+                        mapping.put(dValue, mappingRef.substring(mappingRef.lastIndexOf("/") + 1));
+                    }
+
+                    discriminatorsMapping.put(componentName, mapping);
+                }
+            }
         }
+
+        tryToExtendsClasses(parentRefs, jcodeModel, configuration.packageName());
+        discriminatorsMapping.forEach((cls, mapping) -> tryToAnnotateJsonSubTypes(cls, mapping, configuration.packageName(), jcodeModel));
+        jcodeModel.build(configuration.outputJavaClassDirectory().toFile());
+    }
+
+    private static void tryToExtendsClasses(Map<String, String> parentRefs, JCodeModel jcodeModel, String packageName){
+        parentRefs.forEach((cls, ref) -> jcodeModel.packages().forEachRemaining(p -> {
+            if (p.name().equals(packageName)) {
+                AtomicReference<JDefinedClass> clsClass = new AtomicReference<>();
+                AtomicReference<JDefinedClass> refClass = new AtomicReference<>();
+
+                p.classes().forEachRemaining(c -> {
+                    if (c.name().equals(cls)) {
+                        clsClass.set(c);
+                    } else if (c.name().equals(ref)) {
+                        refClass.set(c);
+                    }
+                });
+
+                if (clsClass.get() != null && refClass.get() != null) {
+                    clsClass.get()._extends(refClass.get());
+
+                }
+            }
+        }));
+    }
+
+    /**
+     * Add JsonTypeInfo annotation if discriminator defined
+     *
+     * @JsonTypeInfo( use = JsonTypeInfo.Id.NAME,
+     * include = JsonTypeInfo.As.EXISTING_PROPERTY,
+     * property = "type",
+     * visible = true,
+     * defaultImpl = AttributeChangedEvent.class
+     * )
+     */
+    private static void tryToAnnotateJsonTypeInfo(JDefinedClass jclass, String propertyName) {
+        Class<JsonTypeInfo> jti = JsonTypeInfo.class;
+        JClass annotationClass = jclass.owner().ref(jti.getName());
+        JAnnotationUse generated = jclass.annotate(annotationClass);
+        generated.param("property", propertyName);
+        generated.param("use", JsonTypeInfo.Id.NAME);
+        generated.param("include", JsonTypeInfo.As.EXISTING_PROPERTY);
+        generated.param("visible", true);
+    }
+
+    /**
+     * add JsonSubTypes annotation to superclass if define discriminator and discrimination mapping
+     *
+     * @JsonSubTypes(value = {
+     * @JsonSubTypes.Type(name = "CHANGED_EVENT", value = ChangedEvent.class),
+     * @JsonSubTypes.Type(name = "DELETED_EVENT", value = DeletedEvent.class)
+     * })
+     */
+    private static void tryToAnnotateJsonSubTypes(String className, Map<String, String> mapping, String packageName, JCodeModel model) {
+        final Map<String, JDefinedClass> classes = new HashMap<>();
+        final Collection<String> mappingClasses = mapping.values();
+
+        model.packages().forEachRemaining(p -> {
+            if (p.name().equals(packageName)) {
+
+                p.classes().forEachRemaining(c -> {
+                    if (c.name().equals(className)) {
+                        classes.put(className, c);
+                    } else if (mappingClasses.contains(c.name())) {
+                        classes.put(c.name(), c);
+                    }
+                });
+            }
+        });
+
+        JDefinedClass parentClass = classes.get(className);
+
+        Class<JsonSubTypes> jst = JsonSubTypes.class;
+        JClass annotationClass = parentClass.owner().ref(jst.getName());
+        JAnnotationUse generated = parentClass.annotate(annotationClass);
+        JAnnotationArrayMember values = generated.paramArray("value");
+
+        Class<JsonSubTypes.Type> jstType = JsonSubTypes.Type.class;
+        JClass annotationClassType = parentClass.owner().ref(jstType.getName());
+
+        mapping.forEach((descriminator, classRef) -> {
+            JAnnotationUse annotate = values.annotate(annotationClassType);
+            annotate.param("name", descriminator);
+            annotate.param("value", classes.get(classRef));
+        });
+
     }
 }
--- springwolfdoc2dto-core/src/test/java/io/github/stepanovd/springwolf2dto/PojoGeneratorTest.java
+++ springwolfdoc2dto-core/src/test/java/io/github/stepanovd/springwolf2dto/PojoGeneratorTest.java
@@ -10,6 +10,7 @@ import javax.tools.*;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.*;
@@ -23,8 +24,10 @@ class PojoGeneratorTest {
     @Test
     void convertJsonToJavaClass() throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Path path = Paths.get("src/test/java");
+        String packageName = "pckg.test";
+        deleteDirectoryRecursive(path.resolve(packageName.split("\\.")[0]));
         Files.createDirectories(path);
-        Configuration config = new Configuration("url", null, path, "pckg.test", "service");
+        Configuration config = new Configuration("url", null, path, packageName, "service");
 
         Connector connector = new StringConnector(
                 """
@@ -46,7 +49,7 @@ class PojoGeneratorTest {
                                 "subscribe": {
                                   "bindings": {
                                     "kafka": {
-                                      
+
                                     }
                                   },
                                   "message": {
@@ -63,7 +66,7 @@ class PojoGeneratorTest {
                                 },
                                 "bindings": {
                                   "kafka": {
-                                    
+
                                   }
                                 }
                               }
@@ -125,7 +128,10 @@ class PojoGeneratorTest {
 
         PojoGenerator.convertJsonToJavaClass(config, connector);
 
-        Path srcPath = Paths.get("src/test/java/pckg/test");
+        Path srcPath = path;
+        for (String subfolder : packageName.split("\\.")) {
+            srcPath = srcPath.resolve(subfolder);
+        }
 
         List<File> files = Files.list(srcPath)
                 .map(Path::toFile)
@@ -160,9 +166,9 @@ class PojoGeneratorTest {
 
         ClassLoader classLoader = PojoGeneratorTest.class.getClassLoader();
         URLClassLoader urlClassLoader = new URLClassLoader(
-                new URL[] {path.toUri().toURL()},
+                new URL[]{path.toUri().toURL()},
                 classLoader);
-        Class testEventClass = urlClassLoader.loadClass("pckg.test.TestEvent");
+        Class<?> testEventClass = urlClassLoader.loadClass("pckg.test.TestEvent");
 
         System.out.println("loaded class");
 
@@ -185,13 +191,269 @@ class PojoGeneratorTest {
         var testObject = objectMapper.readValue(exampleObject, testEventClass);
 
         Assertions.assertEquals(testEventClass.getMethod("getId").invoke(testObject), "qwerty");
-        Assertions.assertEquals(testEventClass.getMethod("getOccuredOn").invoke(testObject), LocalDateTime.of(2015, 7, 20, 15, 49, 04));
+        Assertions.assertEquals(testEventClass.getMethod("getOccuredOn").invoke(testObject), LocalDateTime.of(2015, 7, 20, 15, 49, 4));
         Assertions.assertEquals(testEventClass.getMethod("getValueType").invoke(testObject).getClass().getName(), "pckg.test.TestEvent$ValueType");
         Assertions.assertEquals(testEventClass.getMethod("getFlags").invoke(testObject).getClass().getName(), "pckg.test.Flags");
 
         System.out.println("deserialized object");
+        deleteDirectoryRecursive(path.resolve(packageName.split("\\.")[0]));
+    }
 
-        Files.walkFileTree(path.resolve("pckg"), new FileVisitor<>() {
+    @Test
+    void convertJsonToJavaClassWithPolymorphism() throws IOException, ClassNotFoundException {
+        Path path = Paths.get("src/test/java");
+        String packageName = "pckg.test";
+        deleteDirectoryRecursive(path.resolve(packageName.split("\\.")[0]));
+
+        Files.createDirectories(path);
+        Configuration config = new Configuration("url", null, path, packageName, "service");
+
+        Connector connector = new StringConnector(
+                """                   
+                        {
+                            "service": {
+                                "asyncapi": "2.0.0",
+                                "info": {
+                                    "title": "service",
+                                    "version": "2.0.1-SNAPSHOT"
+                                },
+                                "servers": {
+                                    "kafka": {
+                                        "url": "localhost:9092",
+                                        "protocol": "kafka"
+                                    }
+                                },
+                                "channels": {
+                                    "kafka-channel": {
+                                        "subscribe": {
+                                            "bindings": {
+                                                "kafka": {}
+                                            },
+                                            "message": {
+                                                "oneOf": [
+                                                    {
+                                                        "name": "pckg.test.DomainEvent",
+                                                        "title": "DomainEvent",
+                                                        "payload": {
+                                                            "$ref": "#/components/schemas/DomainEvent"
+                                                        }
+                                                    },
+                                                    {
+                                                        "name": "pckg.test.ChangedEvent",
+                                                        "title": "ChangedEvent",
+                                                        "payload": {
+                                                            "$ref": "#/components/schemas/ChangedEvent"
+                                                        }
+                                                    },
+                                                    {
+                                                        "name": "pckg.test.DeletedEvent",
+                                                        "title": "DeletedEvent",
+                                                        "payload": {
+                                                            "$ref": "#/components/schemas/DeletedEvent"
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        },
+                                        "bindings": {
+                                            "kafka": {}
+                                        }
+                                    }
+                                },
+                                "components": {
+                                    "schemas": {
+                                        "ChangedEvent": {
+                                            "required": [
+                                                "id",
+                                                "occuredOn",
+                                                "value",
+                                                "type"
+                                            ],
+                                            "type": "object",
+                                            "properties": {
+                                                "id": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "occuredOn": {
+                                                    "type": "string",
+                                                    "format": "date-time",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "value": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "type": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                }
+                                            },
+                                            "example": {
+                                                "id": "string",
+                                                "occuredOn": "2015-07-20T15:49:04",
+                                                "value": "string",
+                                                "type": "CHANGED_EVENT"
+                                            },
+                                            "exampleSetFlag": true
+                                        },
+                                        "DeletedEvent": {
+                                            "type": "object",
+                                            "properties": {
+                                                "id": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "occuredOn": {
+                                                    "type": "string",
+                                                    "format": "date-time",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "valueId": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "type": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                }
+                                            },
+                                            "example": {
+                                                "id": "string",
+                                                "occuredOn": "2015-07-20T15:49:04",
+                                                "valueId": "string",
+                                                "type": "DELETED_EVENT"
+                                            },
+                                            "exampleSetFlag": true
+                                        },
+                                        "DomainEvent": {
+                                            "type": "object",
+                                            "properties": {
+                                                "id": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "occuredOn": {
+                                                    "type": "string",
+                                                    "format": "date-time",
+                                                    "exampleSetFlag": false
+                                                },
+                                                "type": {
+                                                    "type": "string",
+                                                    "exampleSetFlag": false
+                                                }
+                                            },
+                                            "example": {
+                                                "id": "string",
+                                                "occuredOn": "2015-07-20T15:49:04",
+                                                "type": "string"
+                                            },
+                                            "discriminator": {
+                                                "propertyName": "type",
+                                                "mapping": {
+                                                    "CHANGED_EVENT": "#/components/schemas/ChangedEvent",
+                                                    "DELETED_EVENT": "#/components/schemas/DeletedEvent"
+                                                }
+                                            },
+                                            "exampleSetFlag": true,
+                                            "oneOf": [
+                                                {
+                                                    "$ref": "#/components/schemas/ChangedEvent",
+                                                    "exampleSetFlag": false
+                                                },
+                                                {
+                                                    "$ref": "#/components/schemas/DeletedEvent",
+                                                    "exampleSetFlag": false
+                                                }
+                                            ]
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                                   
+                        """
+        );
+
+        PojoGenerator.convertJsonToJavaClass(config, connector);
+
+        Path srcPath = path;
+        for (String subfolder : packageName.split("\\.")) {
+            srcPath = srcPath.resolve(subfolder);
+        }
+
+        List<File> files = Files.list(srcPath)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(files);
+
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+
+        JavaCompiler.CompilationTask task = compiler.getTask(
+                null,
+                fileManager,
+                diagnostics,
+                null,
+                null,
+                compilationUnits
+        );
+
+        task.call();
+
+        for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
+            System.out.format("Error on line %d in %s%n",
+                    diagnostic.getLineNumber(),
+                    diagnostic.getSource());
+        }
+
+        fileManager.close();
+
+        ClassLoader classLoader = PojoGeneratorTest.class.getClassLoader();
+        URLClassLoader urlClassLoader = new URLClassLoader(
+                new URL[]{path.toUri().toURL()},
+                classLoader);
+        Class<?> domainEventClass = urlClassLoader.loadClass("pckg.test.DomainEvent");
+
+        System.out.println("loaded class");
+
+        String exampleChangedEvent = """
+                     {
+                        "id": "string",
+                        "occuredOn": "2015-07-20T15:49:04",
+                        "value": "string",
+                        "type": "CHANGED_EVENT"
+                    }
+                """;
+        String exampleDeletedEvent = """
+                     {
+                        "id": "string",
+                        "occuredOn": "2015-07-20T15:49:04",
+                        "valueId": "string",
+                        "type": "DELETED_EVENT"
+                    }
+                """;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        var testChangedObject = objectMapper.readValue(exampleChangedEvent, domainEventClass);
+        var testDeletedObject = objectMapper.readValue(exampleDeletedEvent, domainEventClass);
+
+        Assertions.assertEquals(testChangedObject.getClass().getName(), "pckg.test.ChangedEvent");
+        Assertions.assertEquals(testDeletedObject.getClass().getName(), "pckg.test.DeletedEvent");
+
+        System.out.println("deserialized object");
+
+        deleteDirectoryRecursive(path.resolve(packageName.split("\\.")[0]));
+    }
+
+    private void deleteDirectoryRecursive(Path path) throws IOException {
+        Files.walkFileTree(path, new FileVisitor<>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
                 return FileVisitResult.CONTINUE;
